93471812fc9bb6237a2466eecca51e2d38e6d802
Asif-Faizal <moh.asif@protonmail.ch>
2025-05-14 14:59:58 +0530

    AUTH DATA STORED

--- Mobile/lib/features/login/data/login/login_repo_impl.dart
+++ Mobile/lib/features/login/data/login/login_repo_impl.dart
@@ -4,6 +4,8 @@ import 'package:versace/features/login/data/login/login_datasource.dart';
 import 'package:versace/features/login/domain/login/entities/login_entity.dart';
 import 'package:versace/features/login/domain/login/login_repo.dart';
 
+import '../../../../core/injection/injection_container.dart';
+import '../../../../core/storage/storage_helper.dart';
 import 'models/login_request_model.dart';
 
 class LoginRepositoryImpl extends LoginRepository {
@@ -13,6 +15,23 @@ class LoginRepositoryImpl extends LoginRepository {
 
   @override
   Future<Either<Failure, LoginEntity>> login(LoginRequestModel request) async {
-    return await remoteDataSource.login(request);
+    final StorageHelper storageHelper = getIt<StorageHelper>();
+    final response = await remoteDataSource.login(request);
+    final accessToken = response.fold(
+      (failure) => null,
+      (entity) => entity.accessToken,
+    );
+    final refreshToken = response.fold(
+      (failure) => null,
+      (entity) => entity.refreshToken,
+    );
+    if (accessToken != null && refreshToken != null) {
+      await storageHelper.setAuthData(
+        isLoggedIn: true,
+        accessToken: accessToken,
+        refreshToken: refreshToken,
+      );
+    }
+    return response.fold((failure) => Left(failure), (entity) => Right(entity));
   }
 }
--- Mobile/lib/features/register/data/register/register_repo_impl.dart
+++ Mobile/lib/features/register/data/register/register_repo_impl.dart
@@ -3,6 +3,8 @@ import 'package:versace/features/register/data/register/register_datasource.dart
 import 'package:versace/features/register/domain/register/register_repo.dart';
 
 import '../../../../core/error/failures.dart';
+import '../../../../core/injection/injection_container.dart';
+import '../../../../core/storage/storage_helper.dart';
 import '../../domain/register/entity/register_entity.dart';
 import 'model/register_request_model.dart';
 
@@ -12,6 +14,23 @@ class RegisterRepoImpl implements RegisterRepository {
 
   @override
   Future<Either<Failure, RegisterEntity>> register(RegisterRequestModel request) async {
-    return await datasource.register(request);
+    final StorageHelper storageHelper = getIt<StorageHelper>();
+    final response = await datasource.register(request);
+    final accessToken = response.fold(
+      (failure) => null,
+      (entity) => entity.accessToken,
+    );
+    final refreshToken = response.fold(
+      (failure) => null,
+      (entity) => entity.refreshToken,
+    );
+    if (accessToken != null && refreshToken != null) {
+      await storageHelper.setAuthData(
+        isLoggedIn: true,
+        accessToken: accessToken,
+        refreshToken: refreshToken,
+      );
+    }
+    return response.fold((failure) => Left(failure), (entity) => Right(entity));
   }
 }
